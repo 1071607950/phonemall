5abe42f0b99e44d48025e846ffb7f5dcdd3b4b62
liuzhongkun <1071607950@qq.com>
2021-05-01 18:43:51 +0800

    项目初始结构创建
    Signed-off-by: liuzk <1071607950@qq.com>

---
+++ phonemall-coupon/src/test/java/com/city/phonemall/coupon/PhonemallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.city.phonemall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhonemallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ phonemall-order/src/main/java/com/city/phonemall/order/PhonemallOrderApplication.java
@@ -0,0 +1,13 @@
+package com.city.phonemall.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhonemallOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhonemallOrderApplication.class, args);
+    }
+
+}
